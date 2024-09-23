84bdb80f7363478260d550cc69c9d31d06e9b269
thekilla0 <omar.djebara14@gmail.com>
2024-09-23 17:02:10 +0100

    stars feature using primeng

--- frontend/src/app/component/pages/home/home.component.ts
+++ frontend/src/app/component/pages/home/home.component.ts
@@ -4,15 +4,18 @@ import { FoodService } from '../../../services/food.service';
 import { NgFor } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgFor, RouterModule, CommonModule],
+  imports: [NgFor, RouterModule, CommonModule, RatingModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   foods: Food[] = [];
+  value: number = 0;
   constructor(private foodService: FoodService) {
     this.foods = foodService.getAll();
   }
